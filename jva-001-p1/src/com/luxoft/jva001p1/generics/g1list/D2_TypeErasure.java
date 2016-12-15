045beb6cb8abf013d1659340ee469027134e3f52
oyushche <oyushche@gmail.com>
2016-12-15 17:43:56 +0200

    + generics finalized

--- jva-001-p1/src/com/luxoft/jva001p1/generics/g1list/D2_TypeErasure.java
+++ jva-001-p1/src/com/luxoft/jva001p1/generics/g1list/D2_TypeErasure.java
@@ -4,11 +4,27 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-public class D2_TypeErasure<T>
+public class D2_TypeErasure<E>
 {
-    public <E> void test(Collection<E> data) { }
+    public <E> void test(Collection<E> data)
+    {
+        System.out.println("D2_TypeErasure#test(Collection<E>)");
 
-    public void test(List<String> data) { }
+        for (E e : data)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void test(List<String> data)
+    {
+        System.out.println("D2_TypeErasure#test(List<String>)");
+
+        for (String s : data)
+        {
+            System.out.println(s);
+        }
+    }
 
     public static void main(String[] args)
     {
