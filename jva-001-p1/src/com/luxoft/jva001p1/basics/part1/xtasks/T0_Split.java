c418fc0bab4c6e13ee19934df4f08481400c58ef
oyushche <uhmateush@gmail.com>
2019-02-05 03:39:46 -0500

    remove answer

--- jva-001-p1/src/com/luxoft/jva001p1/basics/part1/xtasks/T0_Split.java
+++ jva-001-p1/src/com/luxoft/jva001p1/basics/part1/xtasks/T0_Split.java
@@ -5,12 +5,5 @@ public class T0_Split
     public static void main(String[] args)
     {
         double randomNumber = 343.36;
-
-        String str = "" + randomNumber;
-
-        int dotIndex = str.indexOf(".");
-
-        System.out.println("whole: " + str.substring(0, dotIndex));
-        System.out.println("fract: " + str.substring(dotIndex + 1));
     }
 }
