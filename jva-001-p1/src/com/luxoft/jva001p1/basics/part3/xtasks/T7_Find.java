36430d5a71bbc80272cead46db1ea536d49d58a1
oyushche <uhmateush@gmail.com>
2019-05-21 02:52:59 -0400

    + cafe

---
+++ jva-001-p1/src/com/luxoft/jva001p1/basics/part3/xtasks/T7_Find.java
@@ -0,0 +1,40 @@
+package com.luxoft.jva001p1.basics.part3.xtasks;
+
+import java.util.Arrays;
+
+public class T7_Find
+{
+    public static void main(String[] args) throws Exception
+    {
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println();
+
+
+        find1(numbers, 100);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    /**
+     *
+     * Method should return index of found value of -1 if not found.
+     *
+     */
+    public static int find1(int[] data, int value)
+    {
+        // TODO YOUR CODE HERE
+        return -1;
+    }
+
+    /**
+     *
+     * Method should return count of found values.
+     *
+     */
+    public static int find2(int[] data, int value)
+    {
+        // TODO YOUR CODE HERE
+        return -1;
+    }
+
+}
